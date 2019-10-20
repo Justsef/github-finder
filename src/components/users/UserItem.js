0b5d5cd1c484c2fd4d682b5fcb0dc0d15984bfc8
Justsef <justsef@gmail.com>
2019-10-20 20:33:39 +0200

    Connect single user page to userItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
-  //or const UserItem = props => {
-  // const { login, avatar_url, html_url } = props.user;
-  return (
-    <div className='card text-center'>
-      <img
-        src={avatar_url}
-        alt=''
-        className='round-img'
-        style={{ width: '60px' }}
-      />
-      <h3>{login}</h3>
+	//or const UserItem = props => {
+	// const { login, avatar_url, html_url } = props.user;
+	return (
+		<div className='card text-center'>
+			<img
+				src={avatar_url}
+				alt=''
+				className='round-img'
+				style={{ width: '60px' }}
+			/>
+			<h3>{login}</h3>
 
-      <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
-          More
-        </a>
-      </div>
-    </div>
-  );
+			<div>
+				<Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+					More
+				</Link>
+			</div>
+		</div>
+	);
 };
 UserItem.propTypes = {
-  user: PropTypes.object.isRequired
+	user: PropTypes.object.isRequired
 };
 
 export default UserItem;
